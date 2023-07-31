69d8e57d5eee48c22ded060e8fd32f634c0555cb
Ravi Teja Natchireddi <daredevilteja@outlook.com>
2023-07-31 14:16:17 +0000

    feat(api): add endpoint for calculating premium rate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 import pprint
 import pandas as pd
-import json
 from flask import Flask, request, jsonify
 from pymongo import MongoClient
 
@@ -29,5 +28,57 @@ def index():
             "message": "server started successfully"}
 
 
+@app.route('/get-premium', methods=["POST"])
+def get_premium():
+    data = request.json
+    adult_ages = []
+    child_ages = []
+    adult_count = data.get("adult_count")
+    pprint.pprint(adult_count)
+    if adult_count != 0:
+        adult_ages = data.get("adult_ages")
+        adult_ages.sort(reverse=True)
+    child_count = data.get("child_count")
+    if child_count != 0:
+        child_ages = data.get("child_ages")
+    sum_insured = data.get("sum_insured")
+    city_tier = data.get("city_tier")
+    tenure_of_insurance = data.get("tenure_of_insurance")
+
+    rate = 0
+
+    for i in range(len(adult_ages)):
+        query = {
+            '$and': [
+                {'SumInsured': sum_insured},
+                {'TierID': city_tier},
+                {'Tenure': tenure_of_insurance},
+                {"Age": adult_ages[i]}
+            ]
+        }
+
+        curr_rate = collection.find_one(query).get("Rate")
+        if i == 0:
+            rate += curr_rate
+            continue
+        rate += curr_rate / 2
+
+    if len(child_ages) != 0:
+        for i in range(len(child_ages)):
+            query = {
+                '$and': [
+                    {'SumInsured': sum_insured},
+                    {'TierID': city_tier},
+                    {'Tenure': tenure_of_insurance},
+                    {"Age": child_ages[i]}
+                ]
+            }
+
+            curr_rate = collection.find_one(query).get("Rate")
+            rate += curr_rate / 2
+
+    return jsonify(rate)
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
